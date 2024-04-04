6864c0da52c57e4c5d51faada324cd88b5c68c9c
shanemarvinmay <shanemarvinmay@gmail.com>
2024-04-03 18:53:46 -0700

    Set up CLI to take in an image and use the model to determine what food it is.

--- myhealth/myHealth.py
+++ myhealth/myHealth.py
@@ -1,15 +1,23 @@
 import typer
 
+import numpy as np
+import tensorflow as tf
+
 app = typer.Typer()
+model_file_path = 'model/mini_model.keras'
+class_names = ['hamburger', 'hot dog', 'pizza']
 
 @app.command()
 def get_nutrition_from_photo(photo_path: str):
     """Upload photo based on file path.
     """    
-    print(f"Photo path: {photo_path}")
-    with open(photo_path, 'rb') as f:
-        photo = f.read()
-    print(f"Photo preview:{photo[:50]}")
+    img = tf.keras.utils.load_img(photo_path)
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    model = tf.keras.models.load_model(model_file_path)
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    print( "This image most likely belongs to {} with a {:.2f} percent confidence." .format(class_names[np.argmax(score)], 100 * np.max(score)) )
 
 if __name__ == "__main__":
     app()
