0f2849f728cddf74f183719864e8483835a4d9e4
shanemarvinmay <shanemarvinmay@gmail.com>
2024-04-08 18:10:59 -0700

    Fixing merger errors.

--- myhealth/myHealth.py
+++ myhealth/myHealth.py
@@ -38,34 +38,6 @@ def print_nutritional_data(nutritional_data):
     print(table)
 
 
-def get_nutritional_data_for_food(food_name):
-    db = TinyDB("data/db.json")
-    Food = Query()
-    result = db.search(Food.name == food_name)
-    if len(result) < 1:
-        print(f"No nutritional data for {food_name}")
-        return {}
-    return result[0]
-
-
-def print_nutritional_data(nutritional_data):
-    # Print food name
-    food_name = nutritional_data.get("name", "UNKNOWN FOOD NAME").title()
-    print(f"\n{food_name}")
-    # Print serving size disclosure
-    print("The following metrics are based on a 100g serving size.")
-    # Print Nutrients
-    table = PrettyTable()
-    table.field_names = ["Nutrient", "Amount"]
-    for attr in nutritional_data:
-        if attr == "name":
-            continue
-        nutrient_name = attr.title()
-        nutrient_value = round(nutritional_data[attr], 4)
-        table.add_row([nutrient_name, nutrient_value])
-    print(table)
-
-
 @app.command()
 def get_nutrition_from_photo(photo_path: str):
     """get_nutrition_from_photo takes in a path to a photo of food, and prints the nutritional information.
