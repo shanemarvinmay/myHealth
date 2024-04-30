c77028a7b06a24de5c8e44dc27ae016061fa41ed
shanemarvinmay <shanemarvinmay@gmail.com>
2024-04-29 17:46:53 -0700

    Include all results from food entry.

--- myhealth/myHealth.py
+++ myhealth/myHealth.py
@@ -4,6 +4,7 @@ from tinydb import TinyDB, Query
 import typer
 import numpy as np
 import tensorflow as tf
+from typing import List
 
 app = typer.Typer()
 model_file_path = "model/mini_model.keras"
@@ -12,23 +13,28 @@ class_names = ["hamburger", "hot dog", "pizza"]
 
 def get_food_name_from_user() -> str:
     food_name = input("Enter food name (type quit to quit):").lower()
-    while True:
-        if food_name == "quit":
-            exit()
-        nutritional_data = get_nutritional_data_for_food(food_name)
-        if nutritional_data != {}:
-            return food_name
-        food_name = input("Enter food name that is spelled correctly (type quit to quit):").lower()
+    if food_name == "quit":
+        exit()
+    return food_name
 
 
-def get_nutritional_data_for_food(food_name: str):
+def get_nutritional_data_for_food(food_name: str) -> List:
     db = TinyDB("data/db.json")
     Food = Query()
-    result = db.search(Food.name == food_name)
-    if len(result) < 1:
-        print(f"No nutritional data for {food_name}")
-        return {}
-    return result[0]
+    results = db.search(Food.name == food_name)
+    if len(results) > 0:
+        return results
+    # Check secondary db.
+    db = TinyDB("data/secondary_db.json")
+    for food in db:
+        for food_id in food:
+            if food_name in food[food_id]["name"]:
+                results.append(food[food_id])
+    if len(results) > 0:
+        return results
+    # No food found. Exiting program
+    print(f"No nutritional data for '{food_name.title()}'")
+    exit()
 
 
 def print_nutritional_data(nutritional_data: dict):
@@ -56,25 +62,25 @@ def get_nutrition_from_photo(photo_path: str):
     Args:
         photo_path (str): a path to a photo of food.
     """
+    # Running the model on the image that was provided.
     img = tf.keras.utils.load_img(photo_path)
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create a batch
     model = tf.keras.models.load_model(model_file_path)
     predictions = model.predict(img_array)
-    print(f"predictions:{predictions}")
     scores = tf.nn.softmax(predictions[0])
-    print(f"scores:{scores}")
     highest_score_highest_idx = np.argmax(scores)
-    print(f"highest_score_highest_idx:{highest_score_highest_idx}")
+
     if scores[highest_score_highest_idx] < 0.8:
-        print("Low confidence. Ask for food entry.")
+        print("Mmmmm... I don't recognize that food by the picture.")
         food_name = get_food_name_from_user()
     else:
         food_name = class_names[highest_score_highest_idx]
         confidence_score = round(100 * np.max(scores), 2)
         print(f"This food is a {food_name} with a {confidence_score}% confidence.")
-    nutritional_data = get_nutritional_data_for_food(food_name)
-    print_nutritional_data(nutritional_data)
+    nutritional_data_results = get_nutritional_data_for_food(food_name)
+    for nutritional_data in nutritional_data_results:
+        print_nutritional_data(nutritional_data)
 
 
 if __name__ == "__main__":
